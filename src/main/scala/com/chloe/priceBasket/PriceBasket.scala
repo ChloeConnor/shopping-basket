6452643e111a2171b627eb4e922e9fa84fed14bc
chloeconnor <chloe.connor@vortexa.com>
2020-03-06 07:49:04 +0000

    no need for case class can use map

--- src/main/scala/com/chloe/priceBasket/PriceBasket.scala
+++ src/main/scala/com/chloe/priceBasket/PriceBasket.scala
@@ -1,19 +1,24 @@
 package com.chloe.priceBasket
 
-import com.chloe.priceBasket.dataTypes.Good
-
 object PriceBasket extends App {
 
-  def discount(good: Good, percentageOff: Double) = {
-    good.price
+  val goods = Map("soup" -> 0.65, "bread" -> 0.8, "milk" -> 1.3, "apple" -> 1.0)
+
+  def discount(good: String, percentageOff: Double) = goods(good) * (percentageOff)
+
+  def offer(numberBought: Int, itemBought: String, discountOn: String, discountApplied: Double): Unit = {
+    goods(itemBought)
   }
 
   override def main(args: Array[String]): Unit = {
 
-    val soup = Good("soup", 0.65)
-    val bread = Good("bread", 0.8)
-    val milk = Good("milk", 1.3)
-    val apples = Good("apples", 1.0)
+    val totalWithoutDiscount: Double = ???
+
+    val discountApples: Double = discount("apple", 0.1)
+
+    val discountOffer: Double = ???
+
+    val totalWithDiscountApplied = totalWithoutDiscount - discountApples - discountOffer
 
   }
 }
--- src/main/scala/com/chloe/priceBasket/dataTypes/Good.scala
+++
@@ -1,3 +0,0 @@
-package com.chloe.priceBasket.dataTypes
-
-case class Good(good: String, price: Double)

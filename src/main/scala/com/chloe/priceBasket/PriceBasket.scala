f8c09004420a9a565838ae983e1d36e9f2736043
chloeconnor <chloe.connor@vortexa.com>
2020-03-08 21:19:21 +0000

    tidy

--- src/main/scala/com/chloe/priceBasket/PriceBasket.scala
+++ src/main/scala/com/chloe/priceBasket/PriceBasket.scala
@@ -1,7 +1,5 @@
 package com.chloe.priceBasket
 
-import java.lang.System
-
 import com.chloe.priceBasket.utils.ReadFile._
 import com.chloe.priceBasket.discounts.CalculateDiscountedGoods._
 
@@ -10,7 +8,7 @@ object PriceBasket extends App {
   override def main(args: Array[String]): Unit = {
 
     if (args.length == 0) println("Please specify at least one item")
-    
+
     val pricesMap = readCSVToMap("src/main/resources/prices.csv")
     val conditionalDiscounts = readCSVToConditionalDiscount(
       "src/main/resources/conditional_discounts.csv"
